dedd89e4db9359b336dc115e3e47c21d597ef9e8
lluisemper <lluisemper@gmail.com>
2021-01-02 14:16:42 +0000

    migrate project to react, lives not working in react verion yet

---
+++ mariogameReact/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MarioGame from "./Components/MarioGame";
+
+function App() {
+  return (
+    <div className="App">
+      <MarioGame />
+    </div>
+  );
+}
+
+export default App;
